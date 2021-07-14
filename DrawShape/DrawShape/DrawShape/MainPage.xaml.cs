8ef131c0fd70ddb29aacba6258fb2cd49343d0c4
BingFengHung <joe94008@gmail.com>
2021-07-14 16:12:08 +0800

    <feat>: 準備畫圖所需的前置步驟

--- DrawShape/DrawShape/DrawShape/MainPage.xaml.cs
+++ DrawShape/DrawShape/DrawShape/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SkiaSharp;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -17,7 +18,35 @@ namespace DrawShape
 
         private void OnCanvaViewPaintSurface(object sender, SkiaSharp.Views.Forms.SKPaintSurfaceEventArgs e)
         {
+            SKImageInfo info = e.Info;
+            SKSurface surface = e.Surface;
 
+            SKCanvas canvas = surface.Canvas;
+
+
+            // 由於是使用笛卡爾座標，因此畫面原點原本在左上角，因次要有一個轉換的函式
+            Point RelateToOriginalPoint(Point point)
+            {
+                // 設定中心點
+                int x = info.Width / 2;
+                int y = info.Height / 2;
+
+                return new Point(point.X + x, -point.Y + y);
+            }
         }
+
+        public SKPaint StrokeColor = new SKPaint
+        {
+            Style = SKPaintStyle.Stroke,
+            Color = SKColors.Blue,
+            StrokeWidth = 4
+        };
+
+        public SKPaint TextColor = new SKPaint
+        {
+            Color = SKColors.Black,
+            TextAlign = SKTextAlign.Center,
+            TextSize = 40
+        };
     }
 }
